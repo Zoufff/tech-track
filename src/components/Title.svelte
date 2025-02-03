2c1fee95df7b5ba23f7269baff1874429ce1fbcf
Soufiane Ayaou <Soufoaneayaou43@gmail.com>
2025-02-03 22:47:00 +0100

    up

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -15,8 +15,10 @@
     h1 {
         font-size: 3em;
         font-weight: bold;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
     h2 {
         font-size: 2em;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
 </style>
