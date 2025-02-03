e83ffd31aa3c7cfeb37aa8421771980e9c95b47f
Soufiane Ayaou <Soufoaneayaou43@gmail.com>
2025-02-03 21:22:06 +0100

    update

--- src/components2/Title.svelte
+++ src/components2/Title.svelte
@@ -7,3 +7,17 @@
     <h2>{year}</h2>
   </div>
   
+  <style>
+    div {
+      text-align: center;
+      margin-bottom: 15px;
+    }
+    h1 {
+      font-size: 24px;
+      font-weight: bold;
+    }
+    h2 {
+      font-size: 20px;
+      color: rgb(0, 0, 0);
+    }
+  </style>
