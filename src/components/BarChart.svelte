e164729de1042f8400e928f127df0bd15b305659
Soufiane Ayaou <Soufoaneayaou43@gmail.com>
2025-02-04 10:14:18 +0100

    up

--- src/components/BarChart.svelte
+++ src/components/BarChart.svelte
@@ -3,6 +3,7 @@
   import { fetchData } from "../lib/Fetch.js"; // fetch-functie importeren
   import * as d3 from "d3";
   import ChartTitle from "./Title.svelte";
+  import Style from "./Style.svelte";
 
   let data = []; // Alle data van de API wordt opgehaald
   let filteredData = []; // gefilterde data die wordt weergegeven in de grafiek
@@ -166,58 +167,4 @@
 {/if}
 
 <!-- maak hier een apart component van -->
-<style>
-  .chart-container {
-    position: relative;
-    width: auto;
-    max-width: 100%;
-    overflow-x: auto;
-    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-    font-size: 1.2em;
-    margin: 2em;
-  }
-
- div{
-  align-items: center;
- }
-
-  .name-label {
-    font-size: 14px;
-    font-weight: bold;
-  }
-
-  .filter-controls {
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-    margin-bottom: 1em;
-    position: sticky;
-    top: 0;
-    z-index: 1000;
-    padding: 10px;
-  }
-
-  input,
-  select,
-  button {
-    margin: 0;
-    padding: 10px;
-    font-size: 14px;
-    border: 2px solid black;
-    background-color: rgba(255, 255, 255, 0);
-  }
-
-  input,
-  select,
-  button {
-    margin: 5px;
-    padding: 10px;
-    font-size: 14px;
-    border: 2px solid black;
-    background-color: rgb(255, 255, 255);
-  }
-
-  input {
-    width: 550px;
-  }
-</style>
+<Style />
---
+++ src/components/Style.svelte
@@ -0,0 +1,56 @@
+<style>
+    .chart-container {
+      position: relative;
+      width: auto;
+      max-width: 100%;
+      overflow-x: auto;
+      font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+      font-size: 1.2em;
+      margin: 2em;
+    }
+  
+   div{
+    align-items: center;
+   }
+  
+    .name-label {
+      font-size: 14px;
+      font-weight: bold;
+    }
+  
+    .filter-controls {
+      display: flex;
+      justify-content: center;
+      gap: 10px;
+      margin-bottom: 1em;
+      position: sticky;
+      top: 0;
+      z-index: 1000;
+      padding: 10px;
+    }
+  
+    input,
+    select,
+    button {
+      margin: 0;
+      padding: 10px;
+      font-size: 14px;
+      border: 2px solid black;
+      background-color: rgba(255, 255, 255, 0);
+    }
+  
+    input,
+    select,
+    button {
+      margin: 5px;
+      padding: 10px;
+      font-size: 14px;
+      border: 2px solid black;
+      background-color: rgb(255, 255, 255);
+    }
+  
+    input {
+      width: 550px;
+    }
+  </style>
+  
